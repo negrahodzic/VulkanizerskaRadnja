587116a19742e7c9403a070a5890a72ef152b79b
negrahodzic <negra.hodzic@gmail.com>
2020-03-10 14:36:26 +0100

    Dodata dokumentacija za klasu VulkanizerskaRadnja

--- VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -3,10 +3,24 @@ package gume.radnja;
 import java.util.LinkedList;
 import gume.AutoGuma;
 
+/**
+ * Vulkanizerska radnja koja vrsi operacije nad auto gumama.
+ * 
+ * @author Negra Hodzic
+ *
+ */
 public class VulkanizerskaRadnja {
-	
+	/**
+	 * Lista auto guma u radnji
+	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
+	/**
+	 * Dodaje gumu u listu auto guma.
+	 * 
+	 * @param a Auto guma koja se dodaje u listu guma
+	 * @throws java.lang.RuntimeException Ako je uneta null vrednst ili guma vec postoji u listi guma
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -15,6 +29,14 @@ public class VulkanizerskaRadnja {
 		gume.addFirst(a);
 	}
 
+	/**
+	 * Pronalazi auto gumu u listi guma na osnovu zadate marke modela gume.
+	 * 
+	 * @param markaModel Marka modela guma na osnovu koje se pretrazuje guma
+	 * @return Ako je marka modela null vraca null u suprotnom vraca se 
+	 * lista guma koja zadovoljava kriterijum marke modela
+	 * 
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
